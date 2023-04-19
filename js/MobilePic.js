238de5a5fcb87f3f558f098289a6a3350851df8e
SeongkookKIM <ksk8646@naver.com>
2023-04-19 14:11:32 +0900

    Project Link & Nft-Title font크기 수정

--- js/MobilePic.js
+++
@@ -1,7 +0,0 @@
-var image = document.getElementById("m-image");
-var images = ["img/picture02.png", "img/picture01.png"];
-var i = 0;
-setInterval(function () {
-  image.src = images[i];
-  i = (i + 1) % images.length;
-}, 300);
--- js/MobilePure.js
+++ js/MobilePure.js
@@ -1,20 +1,20 @@
 // @@@PROJECT-LIST
-const projectListItems = document.querySelectorAll(".m-project-list");
+// const projectListItems = document.querySelectorAll(".m-project-list");
 const picList = document.querySelectorAll(".m-pic");
 
-projectListItems.forEach(function (it) {
-  it.addEventListener("mouseover", function () {
-    const newImageSrc = this.getAttribute("data-hover-bg");
-    const imageElement = this.querySelector("img");
-    imageElement.src = newImageSrc;
-  });
+// projectListItems.forEach(function (it) {
+//   it.addEventListener("mouseover", function () {
+//     const newImageSrc = this.getAttribute("data-hover-bg");
+//     const imageElement = this.querySelector("img");
+//     imageElement.src = newImageSrc;
+//   });
 
-  it.addEventListener("mouseout", function () {
-    const originalImageSrc = this.getAttribute("data-original-bg");
-    const imageElement = this.querySelector("img");
-    imageElement.src = originalImageSrc;
-  });
-});
+//   it.addEventListener("mouseout", function () {
+//     const originalImageSrc = this.getAttribute("data-original-bg");
+//     const imageElement = this.querySelector("img");
+//     imageElement.src = originalImageSrc;
+//   });
+// });
 
 picList.forEach(function (it) {
   it.addEventListener("mouseover", function () {
